f70965ee59bde34d2e454cc00d02a95ba998e800
Mayke Alisson <maykealison@gmail.com>
2021-02-11 07:24:54 -0300

    init next js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,31 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import db from '../db.json';
+
+const BackgroudImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`;
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+     <BackgroudImage>
+         <QuizContainer>
+             <h1 style={{color: "red"}}> HOME </h1>
+         </QuizContainer>
+      </BackgroudImage>
+  )
 }
