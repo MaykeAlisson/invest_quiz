7787f0165bce046d3d62a18ff38fbd597884e9c1
Mayke Alisson <mayke.furtado@arcom.com.br>
2021-02-11 13:37:06 -0300

    create structure

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,17 @@ import styled from 'styled-components'
 
 import db from '../db.json';
 
-const BackgroudImage = styled.div`
-  background-image: url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
-`;
+import Widget from '../src/components/Widget';
+import QuizBackground from "../src/components/QuizBackgroud";
+import Footer from "../src/components/Footer";
+import GitHubCorner from "../src/components/GitHubCorner";
+
+// const BackgroudImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
 
 const QuizContainer = styled.div`
   width: 100%;
@@ -20,12 +25,22 @@ const QuizContainer = styled.div`
   }
 `;
 
+
 export default function Home() {
-  return (
-     <BackgroudImage>
-         <QuizContainer>
-             <h1 style={{color: "red"}}> HOME </h1>
-         </QuizContainer>
-      </BackgroudImage>
-  )
+    return (
+        <QuizBackground backgroundImage={db.bg}>
+            <QuizContainer>
+                <Widget>
+                    <Widget.Header>
+                        <h1 style={{color: "red"}}> HOME </h1>
+                    </Widget.Header>
+                    <Widget.Content>
+                        texto do corpo
+                    </Widget.Content>
+                </Widget>
+                <Footer/>
+            </QuizContainer>
+            <GitHubCorner projectUrl={'https://github.com/MaykeAlisson'}/>
+        </QuizBackground>
+    )
 }
